5b45503fd7f68d211bfb582a50b1dde57981f3cd
unknown <jatinsharma6204063696@gmail.com>
2023-10-27 15:00:13 +0530

    completed

---
+++ script.js
@@ -0,0 +1,168 @@
+//locomotive scroll trigger codepen code
+
+
+function locomotiveAnimations(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+
+locomotiveAnimations();
+
+function navbarAnimation(){
+    gsap.to("#nav-left svg",{
+        transform:"translateY(-100%)",
+        scrollTrigger:{
+            trigger:"#page1",
+            scroller:"#main",
+            start:"top 0",
+            end:"top -5%",
+            // speed of animation + also on returning to top it changes again
+            scrub:true
+        }
+    })
+    gsap.to("#nav-right #links",{
+        transform:"translateY(-100%)",
+        opacity:0,
+        scrollTrigger:{
+            trigger:"#page1",
+            scroller:"#main",
+            start:"top 0",
+            end:"top -5%",
+            // speed of animation
+            scrub:true
+        }
+    })
+}
+
+navbarAnimation();
+
+
+
+
+
+// playbtn animation on video
+function videoconAnimation(){
+    const videocon=document.getElementById("video-container")
+    const playbtn=document.getElementById("play")
+    videocon.addEventListener("mouseenter",function(){
+      gsap.to(playbtn,{
+          scale:0.5,
+          opacity:0.5
+      })
+  
+    })
+    videocon.addEventListener("mouseleave",function(){
+      gsap.to(playbtn,{
+          scale:0,
+          opacity:0
+      })
+    })  
+    videocon.addEventListener("mousemove",function(dets){
+      gsap.to(playbtn,{
+          left:dets.x-70,
+          top:dets.y-70
+      })
+    })
+}
+videoconAnimation();
+
+//on load page change the course animation
+function onLoadAnimation(){
+    gsap.from("#page1 h1",{
+        y:100,
+        opacity:0,
+        delay:0.5,
+        duration:0.9,
+        stagger:0.3
+    })
+    gsap.from("#page1 #video-container",{
+        y:100,
+        // scale:0.5,
+        opacity:0,
+        delay:1.1,
+        duration:0.9,
+        // stagger:0.3
+    })
+}
+onLoadAnimation()
+
+
+
+// document.querySelectorAll(".child").addEventListener("mouseenter",function(){
+//     gsap.to("#cursor",{
+//         transform: 'translate(-50%,-50%) scale(1)'
+//     })
+// })
+// document.querySelectorAll(".child").addEventListener("mouseleave",function(){
+//     gsap.to("#cursor",{
+//         transform: 'translate(-50%,-50%) scale(0)'
+//     })
+// })
+
+
+
+// A
+// |
+// |
+
+// same code but for specific part only
+
+// |
+// |
+// v
+
+
+
+//cursor pitch color animation
+function cursorAnimationDarkCirlcle(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#cursor",{
+            left:dets.x,
+            top:dets.y
+        })
+    })
+    let cursorChild=document.querySelectorAll(".child");
+    cursorChild.forEach(child=>{
+        child.addEventListener('mouseenter', function (e) {
+            gsap.to("#cursor",{
+                        transform: 'translate(-50%,-50%) scale(1)'
+                    })
+        })
+    })
+    cursorChild.forEach(child=>{
+        child.addEventListener('mouseleave', function (e) {
+            gsap.to("#cursor",{
+                        transform: 'translate(-50%,-50%) scale(0)'
+                    })
+        })
+    })
+}
+cursorAnimationDarkCirlcle();
